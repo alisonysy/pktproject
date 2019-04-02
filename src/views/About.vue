9ab67bd047d17c9e24abce51a7037715b3acb0ea
alisonys <qwen735@aucklanduni.ac.nz>
2019-04-02 23:39:21 +0800

    init project

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button class="upload-button button" type="button" @click="upload">Upload</button>
+    <image-compressor :done="getFiles" :scale="scale" :quality="quality"></image-compressor>
+    <div class="text-center" v-if="img">
+      <img v-if="img" src="" alt="" :style="{ maxWidth: originalSize ? '100%' : null }" :src="img">
+    </div>
   </div>
 </template>
+
+<script>
+import imageCompressor from "vue-image-compressor";
+export default {
+  data() {
+    return {
+      img: "",
+      scale: 100,
+      quality: 50,
+      originalSize: true,
+      original: {},
+      compressed: {}
+    };
+  },
+  components: { imageCompressor },
+  methods: {
+    upload() {
+      let compressor = this.$refs.compressor.$el;
+      console.log(this.$refs.compressor);
+      console.log(this.$refs.compressor.$el)
+      compressor.click();
+    },
+    getFiles(obj) {
+      console.log(obj);
+      this.img = obj.compressed.blob;
+      this.original = obj.original;
+      this.compressed = obj.compressed;
+    }
+  }
+};
+</script>
