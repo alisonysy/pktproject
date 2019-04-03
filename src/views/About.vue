91aa58ce74820bde23781e17c0e713ae529681aa
alisonys <qwen735@aucklanduni.ac.nz>
2019-04-03 09:45:32 +0800

    image showing

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,7 +10,7 @@
 <script>
 import imageCompressor from "vue-image-compressor";
 export default {
-  data() {
+  /*data() {
     return {
       img: "",
       scale: 100,
@@ -19,7 +19,7 @@ export default {
       original: {},
       compressed: {}
     };
-  },
+  },*/
   components: { imageCompressor },
   methods: {
     getFiles(obj) {
